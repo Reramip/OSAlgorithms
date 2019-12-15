54ec6c8cad744d8290cb1a5ec8f91e612c4279e0
Reramip <Reramip@users.noreply.github.com>
2019-12-15 21:39:27 +0800

    哲学家就餐

---
+++ src/threadsynchronization/DinningPhilosopher.java
@@ -0,0 +1,51 @@
+package threadsynchronization;
+
+import java.util.concurrent.Semaphore;
+
+class Philosopher implements Runnable {
+    int number;
+
+    Philosopher(int number) {
+        this.number = number;
+    }
+
+    @SuppressWarnings("InfiniteLoopStatement")
+    @Override
+    public void run() {
+        while(true){
+            try {
+                System.out.printf("哲学家%d思考\n", number);
+                Thread.sleep(10);
+                int first=Math.min(number%5,(number+1)%5);
+                int second=Math.max(number%5,(number+1)%5);
+                // 先左后右拿
+                DinningPhilosopher.forks[first].acquire();
+                DinningPhilosopher.forks[second].acquire();
+                System.out.printf("哲学家%d吃饭\n",number);
+                Thread.sleep(10);
+                // 先右后左放
+                DinningPhilosopher.forks[second].release();
+                DinningPhilosopher.forks[first].release();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+        }
+    }
+}
+
+public class DinningPhilosopher {
+    public static Semaphore[] forks = new Semaphore[]{
+            new Semaphore(1),
+            new Semaphore(1),
+            new Semaphore(1),
+            new Semaphore(1),
+            new Semaphore(1)
+    };
+
+    public static void main(String[] args) {
+        for (int i = 0; i < 5; ++i) {
+            new Thread(new Philosopher(i)).start();
+        }
+    }
+}
